e16f27b9242cb33868e545f525b040e157884739
Edil Joey <ediljoey2002@gmail.com>
2021-07-23 16:52:20 +0530

    hero banner content updated

--- app/components/HeroBanner/herobanner-content.js
+++ app/components/HeroBanner/herobanner-content.js
@@ -5,29 +5,29 @@ export const ContentItems = [
     contentPosition: 'left',
     Items: [
       {
-        Name: 'Macbook Pro',
+        Name: 'Cross Image',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/cross.jpg?raw=true',
       },
       {
-        Name: 'iPhone',
+        Name: 'New Church',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/new-church.jpg?raw=true',
       },
     ],
   },
   {
-    Name: 'New Church Construction',
-    Caption: 'Say no to manual home labour!',
+    Name: 'Our Old Church',
+    Caption: 'Our Olden History',
     contentPosition: 'middle',
     Items: [
       {
-        Name: 'test test test',
+        Name: 'Old Church',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/old-chirch.jpg?raw=true',
       },
       {
-        Name: 'Learus Vacuum Cleaner',
+        Name: 'Old Church',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/old-church-day.jpg?raw=true',
       },
@@ -39,12 +39,12 @@ export const ContentItems = [
     contentPosition: 'right',
     Items: [
       {
-        Name: 'Living Room Lamp',
+        Name: 'Inside Old Church',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/old-inside.jpg?raw=true',
       },
       {
-        Name: 'Floral Vase',
+        Name: 'Pope',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/pope.png?raw=true',
       },
@@ -56,42 +56,43 @@ export const ContentItems = [
     contentPosition: 'left',
     Items: [
       {
-        Name: 'Macbook Pro',
+        Name: 'Temporary Church Image',
         Image:
           'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/temp.jpg?raw=true',
       },
       {
-        Name: 'iPhone',
-        Image: 'https://source.usplash.com/featured/?iphone',
+        Name: 'Pope.Jorge Mario Bergoglio',
+        Image:
+          'https://github.com/mjashley-academy/mangala-annai-church/blob/04-hp-carousel-section/app/components/HeroBanner/images/pope.png?raw=true',
       },
     ],
   },
   {
     Name: 'New Church Construction',
-    Caption: 'Say no to manual home labour!',
+    Caption: 'Are you willing to donate ! Click Here',
     contentPosition: 'middle',
     Items: [
       {
-        Name: 'test test test',
+        Name: "Our Click's while Image Going",
         Image: 'https://source.unsplash.com/featured/?washingmachine',
       },
       {
-        Name: 'Learus Vacuum Cleaner',
+        Name: 'Work Going on..',
         Image: 'https://source.unsplash.com/featured/?vacuum,cleaner',
       },
     ],
   },
   {
-    Name: 'Decoratives',
-    Caption: 'Give style and color to your living room!',
+    Name: 'Welcome to Kattuvillai',
+    Caption: 'Image',
     contentPosition: 'right',
     Items: [
       {
-        Name: 'Living Room Lamp',
+        Name: 'Image',
         Image: 'https://source.unsplash.com/featured/?lamp',
       },
       {
-        Name: 'Floral Vase',
+        Name: 'Image',
         Image: 'https://source.unsplash.com/featured/?vase',
       },
     ],
